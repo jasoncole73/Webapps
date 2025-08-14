d8fb0587c85e7fd89a7386d677109a935e8ab8aa
jasoncole73 <jasoncole73@gmail.com>
2025-08-14 19:20:23 -0400

    Update config.js

--- weather-flip/config.js
+++ weather-flip/config.js
@@ -1,2 +1,2 @@
 // This placeholder will be ignored if you replace it with your own config.js
-// window.WEATHER_API_KEY = "YOUR_OPENWEATHERMAP_API_KEY_HERE";
+// window.WEATHER_API_KEY = "3ea0446de4024bbbe4e9df1d9c587f64";
